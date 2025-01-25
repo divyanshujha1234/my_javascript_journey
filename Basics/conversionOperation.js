d98d507eef57084760c907492620eaa354a9ac78
divyanshu jha <divyanshu3636@gmail.com>
2025-01-25 13:45:25 +0530

    conversion

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = "undefined";
+
+// console.log(score, typeof(score)); //36 string
+
+// let score_new= Number(score); //N capital
+
+// console.log(score, score_new, typeof(score_new)); //36 36 number
+
+// // Now checking for different cases
+
+// console.log(score , score_new , typeof(score_new)); //36abc NaN number
+
+// console.log(score , score_new , typeof(score_new)); //null NaN number
+
+// console.log(score , score_new , typeof(score_new)); //undefined NaN number
+
+
+// let isLoggedIn= "0";
+// let boolIsLoggedIn= Boolean(isLoggedIn);
+
+// console.log(isLoggedIn, boolIsLoggedIn, typeof(boolIsLoggedIn)); 
+
+// 0 , null , undefined ,"" => false
+// 1, any other number , any string => true
+
+let num= 1;
+
+let inString= String(num);
+
+console.log(num, inString ,typeof(inString)); //everything normal in this
+
+
+
+
+
+
+
+
+
+
+
