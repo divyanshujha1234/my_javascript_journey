5e5c88bdc7e2961a6758e1d87a4e99c392a97283
divyanshu jha <divyanshu3636@gmail.com>
2025-01-25 22:49:02 +0530

    strings

---
+++ Basics/strings.js
@@ -0,0 +1,21 @@
+const name = "Divyanshu Jha";
+const age = 22;
+
+//1st meathod tatti 
+console.log("hello my name is " + name + " and i am " + age + " years old");
+
+//2nd method and recommended
+console.log(`hello my name is ${name} and i am ${age} years old`);
+
+
+//declartion of string
+
+const myName= new String("Ashish Jha");
+
+// myName.substring();
+// myName.slice();
+// myName.trim();
+// myName.replace();
+// myName.split();
+// read from mdn
+
