0e73a7332a3cd7de1607b13e852db3f6388ff26f
divyanshu jha <divyanshu3636@gmail.com>
2025-01-25 14:46:06 +0530

    Datatype summary

---
+++ Basics/data_types_summary.js
@@ -0,0 +1,29 @@
+
+// **Primitive 
+
+// 7 types-
+// string , number, boolean , null , undefined , symbol , BigInt
+
+// **Reference (Non Primitive)
+// 3 types-
+// Array, Object, Functions
+
+let arr= ["divyanshu", 22, true];
+
+let myObj={
+    name: "Divyanshu",
+    age: 22
+};
+
+const myfunc= function(){
+    console.log(" hi! function succesfully called");
+};
+ 
+console.log(arr,myObj,myfunc());
+
+// hi! function succesfully called
+// [ 'divyanshu', 22, true ] { name: 'Divyanshu', age: 22 } undefined
+
+//console.log(typeof(myfunc));
+
+
