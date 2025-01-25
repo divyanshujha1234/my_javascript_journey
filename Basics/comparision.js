e5d38f3ee69b6ae244996f094b48a16abdbae958
divyanshu jha <divyanshu3636@gmail.com>
2025-01-25 14:14:07 +0530

    comparision

---
+++ Basics/comparision.js
@@ -0,0 +1,34 @@
+// console.log(2>1);
+// console.log(2<=1);
+// console.log(2==1); // equality 
+// console.log(2!=1); simple
+
+
+// console.log("2" > 1); //true
+// console.log("02" > 1);//true
+
+console.log(null > 0);   //false
+console.log(null == 0);  //false
+console.log(null >= 0);  //true
+
+// beacause comparison converts treats null as 0 but equality check treats as NaN
+
+//undefined always give false in above cases
+
+
+// === strict check
+
+console.log("2"==2);    //true
+console.log("2" === 2); //false
+
+
+
+
+
+
+
+
+
+
+
+
