750e9bdb1fdad68e21e3076059b873cbbb74b1aa
divyanshu jha <divyanshu3636@gmail.com>
2025-01-25 12:33:44 +0530

    Data_types

---
+++ Basics/data_types.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS code as newer version
+
+// number => 2^53
+// string => ""
+// Boolean
+// null => standalone value 
+// undefined 
+// symbol=> unique 
+// These are all primitive data type
+
+console.log(typeof undefined); // undefined
+console.log (typeof null); // object
